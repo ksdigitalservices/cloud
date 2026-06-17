d24c3d782f1901f69e181b77a997b11d83c6666c
ksdigitalservices <ksdigital2025@gmail.com>
2026-06-17 11:42:47 +0530

    Create apps.bat

---
+++ apps.bat
@@ -0,0 +1,6 @@
+@echo off
+
+start "" "https://www.google.com/chrome/index.html"
+start "" "https://github.com/microsoft/PowerToys/releases/download/v0.100.0/PowerToysUserSetup-0.100.0-x64.exe"
+
+exit
